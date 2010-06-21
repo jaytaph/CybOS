8558dfef541efb272a69296e7b6fc8b6472da4ad
Joshua Thijssen <jthijssen@noxlogic.nl>
2010-06-20 20:15:43 -0400

    Added ELF header parser tool

---
+++ source/tools/elf/elf.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "elf.h"
+
+
+
+int elf_add_section(unsigned int flags, unsigned long addr, unsigned long size, unsigned long offset) {
+  printf ("elf_add_section (f: %04X a: %08X s: %08X o: %08X)\n", flags, addr, size, offset);
+  return 0;
+}
+
+
+/**
+ *
+ */
+int main (void) {
+  char buf[512];
+  int i;
+
+  // Open file
+  FILE *f = fopen ("init.bin", "rb");
+  if (!f) return 0;
+
+  // Read 1st sector
+  fseek (f, 0, SEEK_SET);
+  fread (buf, 1, sizeof (elf32_ehdr), f);
+
+  elf32_ehdr *hdr = (elf32_ehdr *)buf;
+
+  printf ("=== ELF HEADER === \n");
+  printf ("INDENT      : "); for (i=0; i!=EI_NIDENT; i++) printf ("%02X ", hdr->e_indent[i]); printf ("\n");
+  printf ("TYPE        : %04X\n", hdr->e_type);
+  printf ("MACHINE     : %04X\n", hdr->e_machine);
+  printf ("VERSION     : %04X\n", hdr->e_version);
+  printf ("ENTRY       : %04X\n", hdr->e_entry);
+  printf ("PHOFF       : %04X\n", hdr->e_phoff);
+  printf ("SHOFF       : %04X\n", hdr->e_shoff);
+  printf ("FLAGS       : %04X\n", hdr->e_flags);
+  printf ("e_ehsize    : %04X\n", hdr->e_ehsize);
+  printf ("e_phentsize : %04X\n", hdr->e_phentsize);
+  printf ("e_phnum     : %04X\n", hdr->e_phnum);
+  printf ("e_shentsize : %04X\n", hdr->e_shentsize);
+  printf ("e_shnum     : %04X\n", hdr->e_shnum);
+  printf ("e_shstrndx  : %04X\n", hdr->e_shstrndx);
+  printf ("\n");
+
+  // Must have ELF-signature
+  if (hdr->e_indent[EI_MAG0] != 0x7F &&
+      hdr->e_indent[EI_MAG1] != 'E' &&
+      hdr->e_indent[EI_MAG2] != 'L' &&
+      hdr->e_indent[EI_MAG3] != 'F') return 0;
+  printf ("Signature found\n");
+
+  // Must be 32bit
+  if (hdr->e_indent[EI_CLASS] != ELFCLASS32) return 0;
+
+  // Must be little endian
+  if (hdr->e_indent[EI_DATA] != ELFDATA2LSB) return 0;
+
+  // Must be current elf spec
+  if (hdr->e_indent[EI_VERSION] != EV_CURRENT) return 0;
+
+  // Must be executable
+  if (hdr->e_type != ET_EXEC) return 0;
+
+  // Must be i386
+  if (hdr->e_machine != EM_386) return 0;
+
+  // Version 2 must also be current elf spec
+  if (hdr->e_version != EV_CURRENT) return 0;
+
+  printf ("Looks good!\n\n");
+
+
+  printf ("Entry point on %08X\n", hdr->e_entry);
+  printf ("Program headers: %d on a size of %d\n", hdr->e_phnum, hdr->e_phentsize);
+  printf ("Section headers: %d on a size of %d\n", hdr->e_shnum, hdr->e_shentsize);
+  printf ("\n\n");
+
+  do_program_headers (f, hdr);
+  do_section_headers (f, hdr);
+
+  fclose (f);
+  printf ("\n");
+  return 0;
+}
+
+/**
+ *
+ */
+int do_program_headers (FILE *f, elf32_ehdr *hdr) {
+  int i,j;
+
+  // Goto start of program headers and read into buffer
+  fseek (f, hdr->e_phoff, SEEK_SET);
+  elf32_phdr *phbuf = malloc (hdr->e_phnum * sizeof (elf32_phdr));
+  fread (phbuf, hdr->e_phnum, sizeof (elf32_phdr), f);
+  elf32_phdr *ph_ptr = phbuf;
+
+  for (i=0; i!=hdr->e_phnum; i++) {
+    printf ("Reading program header %d\n", i);
+    printf ("p_type     : %04X\n", ph_ptr->p_type);
+    printf ("p_offset   : %04X\n", ph_ptr->p_offset);
+    printf ("p_vaddr    : %04X\n", ph_ptr->p_vaddr);
+    printf ("p_paddr    : %04X\n", ph_ptr->p_paddr);
+    printf ("p_filesz   : %04X\n", ph_ptr->p_filesz);
+    printf ("p_memsz    : %04X\n", ph_ptr->p_memsz);
+    printf ("p_flags    : %04X\n", ph_ptr->p_flags);
+    printf ("p_align    : %04X\n", ph_ptr->p_align);
+
+    switch (ph_ptr->p_type) {
+      default         :
+      case PT_NULL    :
+      case PT_INTERP  :
+      case PT_NOTE    :
+      case PT_PHDR    :
+      case PT_LOPROC  :
+      case PT_HIPROC  :
+                         // Do nothing
+                         printf ("Skipping\n");
+                         break;
+      case PT_DYNAMIC :
+      case PT_SHLIB   :
+                         // Cannot handle these yet.
+                         printf ("Cannot handle this...\n");
+                         return 0;
+                         break;
+      case PT_LOAD    :
+                         j = elf_add_section (1 | (ph_ptr->p_type & 0x7),
+                                              ph_ptr->p_vaddr,
+                                              ph_ptr->p_filesz,
+                                              ph_ptr->p_offset);
+                         if (j != 0) return 0;
+
+                         /* The memory size is larger than the filesize. Fill everything
+                          * up with zero's (ie: BSS segment) */
+                         if (ph_ptr->p_memsz > ph_ptr->p_filesz) {
+                           j = elf_add_section (0 | (ph_ptr->p_type & 0x7),
+                                                ph_ptr->p_vaddr + ph_ptr->p_filesz,
+                                                ph_ptr->p_memsz - ph_ptr->p_filesz,
+                                                ph_ptr->p_offset);
+                         }
+                         break;
+    }
+    ph_ptr++;
+  }
+
+  free (phbuf);
+  printf ("\n\n");
+}
+
+/**
+ *
+ */
+int do_section_headers (FILE *f, elf32_ehdr *hdr) {
+  int i,j;
+
+  // Goto start of program headers and read into buffer
+  fseek (f, hdr->e_shoff, SEEK_SET);
+  elf32_shdr *shbuf = malloc (hdr->e_shnum * sizeof (elf32_shdr));
+  fread (shbuf, hdr->e_shnum, sizeof (elf32_shdr), f);
+  elf32_shdr *sh_ptr = shbuf;
+
+  for (i=0; i!=hdr->e_shnum; i++) {
+    printf ("Reading section header %d\n", i);
+    printf ("sh_type      : %04X\n", sh_ptr->sh_type);
+    printf ("sh_flags     : %04X\n", sh_ptr->sh_flags);
+    printf ("sh_addr      : %04X\n", sh_ptr->sh_addr);
+    printf ("sh_offset    : %04X\n", sh_ptr->sh_offset);
+    printf ("sh_size      : %04X\n", sh_ptr->sh_size);
+    printf ("sh_link      : %04X\n", sh_ptr->sh_link);
+    printf ("sh_info      : %04X\n", sh_ptr->sh_info);
+    printf ("sh_addralign : %04X\n", sh_ptr->sh_addralign);
+    printf ("sh_entsize   : %04X\n", sh_ptr->sh_entsize);
+
+    sh_ptr++;
+  }
+
+  free (shbuf);
+  printf ("\n\n");
+}
+
+
---
+++ source/tools/elf/elf.h
@@ -0,0 +1,199 @@
+#ifndef __ELF_H__
+#define __ELF_H__
+
+  typedef unsigned char           Uint8;
+  typedef unsigned short int      Uint16;
+  typedef unsigned long int       Uint32;
+  typedef unsigned long long      Uint64;
+
+  typedef signed char             Sint8;        // 8 bits signed
+  typedef signed short int        Sint16;       // ...
+  typedef signed long int         Sint32;
+  typedef signed long long        Sint64;
+
+// Global typedefs
+typedef  Uint32   elf32_addr;
+typedef  Uint16   elf32_half;
+typedef  Uint32   elf32_off;
+typedef  Sint32   elf32_sword;
+typedef  Uint32   elf32_word;
+
+
+#define   ET_NONE    0      // No type
+#define   ET_REL     1      // Relocatable file
+#define   ET_EXEC    2      // Executable file
+#define   ET_DYN     3      // Shared object
+#define   ET_CORE    4      // Core file
+#define   ET_LOPROC  0xFF00 // Processor specific
+#define   ET_HIPROC  0xFFFF // Processor specific
+
+#define   EM_NONE    0    // No machine
+#define   EM_M32     1    // AT&T WE 32100
+#define   EM_SPARC   2    // Sparc
+#define   EM_386     3    // Intel 80386
+#define   EM_68K     4    // Motorola 68000
+#define   EM_88K     5    // Motorola 88000
+#define   EM_860     7    // Intel 80860
+#define   EM_MIPS    8    // MIPS RS3000
+
+#define   EV_NONE    0    // Invalid version
+#define   EV_CURRENT 1    // Current version
+
+#define   EI_MAG0        0
+#define   EI_MAG1        1
+#define   EI_MAG2        2
+#define   EI_MAG3        3
+#define   EI_CLASS       4
+#define   EI_DATA        5
+#define   EI_VERSION     6
+#define   EI_PAD         7
+#define   EI_NIDENT     16
+
+#define   ELFCLASSNONE    0
+#define   ELFCLASS32      1
+#define   ELFCLASS64      2
+
+#define   ELFDATANONE     0
+#define   ELFDATA2LSB     1
+#define   ELFDATA2MSB     2
+
+#define   SHN_UNDEF       0
+#define   SHN_LORESERVE   0xFF00
+#define   SHN_LOPROC      0xFF00
+#define   SHN_HIPROC      0xFF1F
+#define   SHN_ABS         0xFFF1
+#define   SHN_COMMON      0xFFF2
+#define   SHN_HIRESERVE   0xFFFF
+
+#define   SHT_NULL        0
+#define   SHT_PROGBITS    1
+#define   SHT_SYMTAB      2
+#define   SHT_STRTAB      3
+#define   SHT_RELA        4
+#define   SHT_HASH        5
+#define   SHT_DYNAMIC     6
+#define   SHT_NOTE        7
+#define   SHT_NOBITS      8
+#define   SHT_REL         9
+#define   SHT_SHLIB      10
+#define   SHT_DYNSYM     11
+#define   SHT_LOPROC     0x70000000
+#define   SHT_HIPROC     0x7FFFFFFF
+#define   SHT_LOUSER     0x80000000
+#define   SHT_HIUSER     0xFFFFFFFF
+
+
+#define   ELF32_ST_BIND(i)   ((i)>>4)
+#define   ELF32_ST_TYPE(i)   ((i)&0xF)
+#define   ELF32_ST_INFO(b,t) ((b)<<4)+((t)&0xF))
+
+#define   STB_LOCAL   0
+#define   STB_GLOBAL  1
+#define   STB_WEAK    2
+#define   STB_LOPROC  13
+#define   STB_HIPROC  15
+
+#define   STT_NOTYPE    0
+#define   STT_OBJECT    1
+#define   STT_FUNC      2
+#define   STT_SECTION   3
+#define   STT_FILE      4
+#define   STT_LOPROC    13
+#define   STT_HIPROC    15
+
+#define   ELF32_R_SYM(i)    ((i)>>8)
+#define   ELF32_R_TYPE(i)   ((unsigned char)(i))
+#define   ELF32_R_INFO(s,t) (((s)<<8)+(unsigned char)(t))
+
+#define   R_386_NONE      0
+#define   R_386_32        1
+#define   R_386_PC32      2
+#define   R_386_GOT32     3
+#define   R_386_PLT32     4
+#define   R_386_COPY      5
+#define   R_386_GLOB_DAT  6
+#define   R_386_JMP_SLOT  7
+#define   R_386_RELATIVE  8
+#define   R_386_GOTOFF    9
+#define   R_386_GOTPC     10
+
+
+#define   PT_NULL         0
+#define   PT_LOAD         1
+#define   PT_DYNAMIC      2
+#define   PT_INTERP       3
+#define   PT_NOTE         4
+#define   PT_SHLIB        5
+#define   PT_PHDR         6
+#define   PT_LOPROC       0x70000000
+#define   PT_HIPROC       0x7FFFFFFF
+
+// Elf header structure
+#pragma pack(1)
+typedef struct {
+  unsigned char     e_indent[EI_NIDENT];
+  elf32_half        e_type;
+  elf32_half        e_machine;
+  elf32_word        e_version;
+  elf32_addr        e_entry;
+  elf32_off         e_phoff;
+  elf32_off         e_shoff;
+  elf32_word        e_flags;
+  elf32_half        e_ehsize;
+  elf32_half        e_phentsize;
+  elf32_half        e_phnum;
+  elf32_half        e_shentsize;
+  elf32_half        e_shnum;
+  elf32_half        e_shstrndx;
+} elf32_ehdr;
+
+#pragma pack(1)
+typedef struct {
+  elf32_word        sh_type;
+  elf32_word        sh_flags;
+  elf32_word        sh_addr;
+  elf32_addr        sh_offset;
+  elf32_off         sh_size;
+  elf32_word        sh_link;
+  elf32_word        sh_info;
+  elf32_word        sh_addralign;
+  elf32_word        sh_entsize;
+} elf32_shdr;
+
+#pragma pack(1)
+typedef struct {
+  elf32_word        st_name;
+  elf32_addr        st_value;
+  elf32_word        st_size;
+  unsigned char     st_info;
+  unsigned char     st_other;
+  elf32_half        st_shndx;
+} elf32_sym;
+
+#pragma pack(1)
+typedef struct {
+  elf32_addr        r_offset;
+  elf32_word        r_info;
+} elf32_rel;
+
+#pragma pack(1)
+typedef struct {
+  elf32_addr        r_offset;
+  elf32_word        r_info;
+  elf32_sword       r_addend;
+} elf32_rela;
+
+#pragma pack(1)
+typedef struct {
+  elf32_word        p_type;
+  elf32_off         p_offset;
+  elf32_addr        p_vaddr;
+  elf32_addr        p_paddr;
+  elf32_word        p_filesz;
+  elf32_word        p_memsz;
+  elf32_word        p_flags;
+  elf32_word        p_align;
+} elf32_phdr;
+
+
+#endif // __ELF_H__
