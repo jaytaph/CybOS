f034d3c4aafc10a9dc396077a72a8eba03d1200d
Joshua Thijssen <jthijssen@noxlogic.nl>
2011-09-06 21:16:07 +0200

    BIG FIX on execve

--- source/kernel/exec.c
+++ source/kernel/exec.c
@@ -20,15 +20,19 @@ int sys_execve (regs_t *r, char *path, char **args, char **environ) {
   Uint32 entrypoint = load_binary_elf (path);
   if (! entrypoint) return 0;
 
-    kprintf ("Entry point: %08X\n", entrypoint);
+  kprintf ("Entry point: %08X\n", entrypoint);
+  
+  // @TODO: Set arguments and set environment in the environment for this process (I guess)
 
+/*
   // Get calling stack frame
+  // @TODO: This stuff is not needed. We manipulate the stack as given by the ISR
   Uint32 stackpointer_offset = (Uint32)_current_task->kstack + KERNEL_STACK_SIZE - (Uint32)r;
   regs_t *context = (regs_t *) ((Uint32)_current_task->kstack + KERNEL_STACK_SIZE - stackpointer_offset);
+*/
 
   /* We return to the start of the newly loaded program instead of after execve. Since we can never
-   * return from an execve(), everything written afterwards is void.. */
-BOCHS_BREAKPOINT
-  context->eip = entrypoint;
+   * return from an execve() since the calling EIP is removed from the stack */
+  r->eip = entrypoint;
   return 0;
 }
